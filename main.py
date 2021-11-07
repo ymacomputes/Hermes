d12bde3251677591fdd8be131382b2517ae3bc30
Amy <amy.collins49@mail.dcu.ie>
2021-11-07 23:49:42 +0000

    Attempt at writing email func

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import discord
+import smtplib
+from email.message import EmailMessage
+from email.headerregistry import Address
+
+def writeEmail(emailContents):
+    msg = EmailMessage()
+
+    msg['Subject'] = "Discord Messages"
+    msg.set_content(emailContents) 
+    print(emailContents)
+    msg['From'] = Address(addr_spec="amy.collins49@mail.dcu.ie")
+    msg['To'] = Address(addr_spec="amy.collins49@mail.dcu.ie")
+
+    s = smtplib.SMTP('localhost')
+    s.send_message(msg)
+    s.quit()
 
 client = discord.Client()
 
@@ -13,5 +29,9 @@ async def on_message(message):
     print("hello")
     if message.content.startswith('hello'):
         await message.channel.send('Hello!')
+    
+    if message.content[:7] == "!email ":
+        writeEmail(message.content[7:])
+        await message.channel.send(message.content[7:])
 
 client.run("--TOKEN CODE--")
