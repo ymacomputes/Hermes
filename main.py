0cd86c5e2706f267119fe6c27130dd6d8e3eb886
Amy Collins <amy.collins49@mail.dcu.ie>
2022-11-07 19:00:16 +0000

    Added dotenv for passwords

--- main.py
+++ main.py
@@ -4,24 +4,27 @@ from email.message import EmailMessage
 from email.headerregistry import Address
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
+from os import getenv
+load_dotenv()
 
 def writeEmail(messageAuthor, emailContents):
-    sender = 'SENDER EMAIL'
-    pwd = 'PASSWORD'
+    sender = getenv('sender_email')
+    pwd = getenv('sender_pwd')
     subject = "Discord"
 
     msg = MIMEMultipart()
     msg['Subject'] = subject
     msg['From'] = sender
-    msg['To'] = 'SENDEE EMAIL'
+    msg['To'] = getenv('sendee_email')
     msg.attach(MIMEText(f"{messageAuthor}-> {emailContents}",'plain'))
 
     # try:
-    server = smtplib.SMTP("DOMAIN", PORT)
+    server = smtplib.SMTP("smtp.gmail.com", 587)
     server.ehlo()
     server.starttls()
     server.login(sender, pwd)
-    server.sendmail(sender, 'SENDEE EMAIL', msg.as_string())
+    server.sendmail(sender, getenv('sendee_email'), msg.as_string())
     server.close()
     print("successfully sent the mail")
     # except Exception as e:
@@ -40,7 +43,9 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-
+    print(message.content)
+    print(message.author)
+    print("hello")
     if message.content.startswith('hello'):
         await message.channel.send('Hello!')
     
@@ -48,4 +53,4 @@ async def on_message(message):
         writeEmail(message.author.display_name, message.content[7:])
         await message.channel.send(message.content[7:])
 
-client.run("TOKEN")
+client.run(getenv('discord_token'))
